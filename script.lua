febbea0018a63c632890e8adfa72b2151c6da268
hgyTUWyeu <123127747+hgyTUWyeu@users.noreply.github.com>
2023-02-01 22:10:05 -0500

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 2788229376 then
+    loadstring(game:HttpGet"https://pastebin.com/raw/JFrBJBZw")()
+    wait(1)
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/hgyTUWyeu/Key_System/main/Protected.lua")()
+  else
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/hgyTUWyeu/Key_System/main/Protected.lua")()
+end
